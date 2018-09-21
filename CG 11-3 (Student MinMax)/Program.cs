6c5528041170edfc574ecd40a9ad98536ed799b5
Kristen Shea <kstar61@live.com>
2018-09-20 22:29:02 -0500

    *

--- CG 11-3 (Student MinMax)/Program.cs
+++ CG 11-3 (Student MinMax)/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CG_11_3__Student_MinMax_
 {
@@ -6,7 +8,32 @@ namespace CG_11_3__Student_MinMax_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Using the text file studentdata.txt write a program that calculates the 
+            //minimum and maximum score for each student. Print out their name as well.Be sure to use a Student class.
+
+
+
+            string[] students = File.ReadAllLines(@"C:\Users\kstar\source\repos\CG 11-2 (Student Grades)
+                                                    \CG 11-2 (Student Grades)\studentdata.txt");
+
+            List<Student> studentData = new List<Student>();
+
+            foreach (string item in students)
+            {
+                studentData.Add(new Student(item));
+
+            }
+
+            foreach (Student student in studentData)
+            {
+                Console.WriteLine($"{student.StudentNameCap()}: \n " +
+                    $"Lowest exam score: {student.MinScore()}\n " +
+                    $"Highest exam score: {student.MaxScore()} ");
+                
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ CG 11-3 (Student MinMax)/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CG_11_3__Student_MinMax_
+{
+    public class Student
+    {
+      
+            public string Name { get; set; }
+
+            public List<int> Scores { get; set; }
+
+            public Student(string studentData)
+            {
+                string[] studentdataSplit = studentData.Split("; ");
+                Scores = new List<int>();
+
+                for (int i = 0; i < studentdataSplit.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Name = studentdataSplit[i];
+                    }
+                    else
+                    {
+                        int score = int.Parse(studentdataSplit[i]);
+                        Scores.Add(score);
+                    }
+
+                }
+
+            }
+
+        public string StudentNameCap()
+        {
+            return char.ToUpper(this.Name[0]) + this.Name.Substring(1);
+        }
+
+        public int MinScore()
+        {
+            int minScore = Scores.First();
+
+            foreach (int score in Scores)
+            {
+                if (score < minScore)
+                    minScore = score;
+
+            }
+            return minScore;
+        }
+
+        public int MaxScore()
+        {
+            int maxScore = 0;
+            foreach (int score in Scores)
+            {
+                if (score > maxScore)
+                    maxScore = score;
+            }
+            return maxScore;
+        }
+
+
+        
+
+
+    }
+}
