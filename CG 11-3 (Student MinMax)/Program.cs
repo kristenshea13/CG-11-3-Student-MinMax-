64d15626cb6f9007b2b36db4932435a2ede87871
Kristen Shea <kstar61@live.com>
2018-09-20 22:01:13 -0500

    Add project files.

---
+++ CG 11-3 (Student MinMax)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_11_3__Student_MinMax_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
